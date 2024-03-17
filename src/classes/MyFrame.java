564be2c7f78f5316ed09ee426d77b2efa337413b
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-17 13:45:47 -0300

    Setting Time

--- src/classes/MyFrame.java
+++ src/classes/MyFrame.java
@@ -1,7 +1,9 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -13,20 +15,43 @@ public class MyFrame extends JFrame {
     String time;
     
     MyFrame() {
-
+        // Shape of the Frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("My Clock Program");
         this.setLayout(new FlowLayout());
         setSize(350, 200);
         setResizable(false);
 
+        // Time format
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
-
         timeLabel = new JLabel();
-        time = timeFormat.format(Calendar.getInstance().getTime());
-        timeLabel.setText(time);
+
+        // Color and details of the Clock 
+        timeLabel.setFont(new Font("Verdana",Font.PLAIN,50));
+        timeLabel.setForeground(new Color(0x00FF00));
+        timeLabel.setBackground(Color.black);
+        timeLabel.setOpaque(true);
 
         this.add(timeLabel);
         setVisible(true);
+
+        setTime();
     };
+
+    public void setTime() {
+        while (true) {
+            // Getting the current time and displaying on the frame
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            try {
+                // Shows the time and the milliseconds
+                Thread.sleep(1000);
+            } 
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 };
