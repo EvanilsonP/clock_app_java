4be54a856f7f1d5ff2e200b81b56c96c8ca9e6f8
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-17 13:58:55 -0300

    Setting Day

--- src/classes/MyFrame.java
+++ src/classes/MyFrame.java
@@ -1,6 +1,5 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -11,8 +10,13 @@ public class MyFrame extends JFrame {
 
     Calendar calendar;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+
     JLabel timeLabel;
+    JLabel dayLabel;
+
     String time;
+    String day;
     
     MyFrame() {
         // Shape of the Frame
@@ -22,27 +26,37 @@ public class MyFrame extends JFrame {
         setSize(350, 200);
         setResizable(false);
 
-        // Time format
+        // Time and Day Format
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
-        timeLabel = new JLabel();
+        dayFormat = new SimpleDateFormat("EEEE");
 
-        // Color and details of the Clock 
+        // Color and details of the Date Label
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Ink Free",Font.PLAIN,35));
+
+        // Color and details of the Time Label 
+        timeLabel = new JLabel();
         timeLabel.setFont(new Font("Verdana",Font.PLAIN,50));
         timeLabel.setForeground(new Color(0x00FF00));
         timeLabel.setBackground(Color.black);
         timeLabel.setOpaque(true);
 
+        // Adding labels to the frame
         this.add(timeLabel);
-        setVisible(true);
+        this.add(dayLabel);
 
+        setVisible(true);
         setTime();
     };
 
     public void setTime() {
         while (true) {
-            // Getting the current time and displaying on the frame
+            // Getting the current time and day and displaying on the frame
             time = timeFormat.format(Calendar.getInstance().getTime());
             timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
 
             try {
                 // Shows the time and the milliseconds
